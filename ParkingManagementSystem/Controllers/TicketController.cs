a82cab311afae9df7e503004b5e7a8a5bdddd5c3
SunnyAckerMan <sunnysas150@gmail.com>
2023-11-22 02:46:29 +0600

    In Ticket Module - 1. Add 2. Edit 3. Delete Created.

--- ParkingManagementSystem/Controllers/TicketController.cs
+++ ParkingManagementSystem/Controllers/TicketController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using ParkingManagementSystem.DBContext;
+using ParkingManagementSystem.Models;
 
 namespace ParkingManagementSystem.Controllers;
 
@@ -10,8 +11,47 @@ public class TicketController : Controller
     {
         _context = context;
     }
+    [HttpGet]
     public IActionResult Index()
     {
+        var ticketList = _context.Tickets.ToList();
+        return View(ticketList);
+    }
+
+    [HttpGet]
+    public IActionResult Create()
+    {
         return View();
+    }
+
+    [HttpPost]
+    public IActionResult Create(Ticket model)
+    {
+        _context.Tickets.Add(model);
+        _context.SaveChanges();
+        return RedirectToAction("Index");
+    }
+
+    [HttpGet]
+    public IActionResult Update(long id)
+    {
+        var existingData = _context.Tickets.FirstOrDefault(p => p.TicketId == id);
+        return View(existingData);
+    }
+
+    [HttpPost]
+    public IActionResult Update(Ticket model)
+    {
+        _context.Tickets.Update(model);
+        _context.SaveChanges();
+        return RedirectToAction("Index");
+    }
+
+    public IActionResult Delete(long id)
+    {
+        var existingData = _context.Tickets.FirstOrDefault(p => p.TicketId == id);
+        _context.Tickets.Remove(existingData);
+        _context.SaveChanges();
+        return RedirectToAction("Index");
     }
 }
